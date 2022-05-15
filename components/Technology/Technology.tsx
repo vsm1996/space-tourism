4b150255d8c650daf6fd6743a4aeaac6f9249de6
Vanessa Martin <vanessa.s.martin96@gmail.com>
2022-05-15 13:40:56 -0700

    feat(Technology): Initial Technology page

--- components/Technology/Technology.tsx
+++ components/Technology/Technology.tsx
@@ -29,57 +29,86 @@ const Technology = ({ content }: any): JSX.Element => {
   const [currentTechnology, changeCurrentTechnology] = useState({
     name: 'Launch vehicle',
     images: {
-      portrait: './assets/technology/image-launch-vehicle-portrait.jpg',
-      landscape: './assets/technology/image-launch-vehicle-landscape.jpg',
+      portrait: '/assets/technology/image-launch-vehicle-portrait.jpg',
+      landscape: '/assets/technology/image-launch-vehicle-landscape.jpg',
     },
     description: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
   })
 
-  const changeTechnology = (newTechnology: number) => {
-    // const destinationData: any = content[newDestination]
-  }
+  const [currentIndex, setIndex] = useState(0)
 
   const handleClick = (e: any) => {
     e.preventDefault()
 
-    changeCurrentTechnology(content[e.target.value])
+    const newIndex = e.target.value
+    const changedCrew = content[newIndex]
+
+    changeCurrentTechnology(changedCrew)
+    setIndex(newIndex)
   }
 
-  const { name, description } = currentTechnology
+  const { name, description, images } = currentTechnology
 
   return (
-    <section>
-      <h5 className={styles.technologyCTA}>
+    <section className={styles.tech}>
+      <h5 className={styles.techCTA}>
         Space Launch 101
       </h5>
-      <div>
-        <ul>
-          {/* Add button element to li */}
-          <li>
-            <button type='button' onClick={handleClick} value={0}>
-              01 Launch vehicle
-            </button>
-          </li>
-          <li>
-            <button type='button' onClick={handleClick} value={1}>
-              02 Spaceport
-            </button>
-          </li>
-          <li>
-            <button type='button' onClick={handleClick} value={2}>
-              03 Space capsule
-            </button>
-          </li>
-        </ul>
-        <div>
-          {/* give navText mixin */}
-          <h3>
-            {name}
-          </h3>
-          <p>
-            {description}
-          </p>
-          <Image src='/destination/image-moon.png' width={100} height={100} />
+      <div className={styles.techContent}>
+        <div className={styles.techTextWrapper}>
+          <ul className={styles.techList}>
+            {/* Add button element to li */}
+            <li className={styles.techListItem}>
+              <button
+                type='button'
+                onClick={handleClick}
+                value={0}
+                className={styles.techListItemBtn}
+              >
+                1
+              </button>
+            </li>
+            <li className={styles.techListItem}>
+              <button
+                type='button'
+                onClick={handleClick}
+                value={1}
+                className={styles.techListItemBtn}
+              >
+                2
+              </button>
+            </li>
+            <li className={styles.techListItem}>
+              <button
+                type='button'
+                onClick={handleClick}
+                value={2}
+                className={styles.techListItemBtn}
+              >
+                3
+              </button>
+            </li>
+          </ul>
+          <div className={styles.techTextContent}>
+            {/* give navText mixin */}
+            <p className={styles.techIntro}>
+              The Terminology...
+            </p>
+            <h3 className={styles.techTitle}>
+              {name}
+            </h3>
+            <p className={styles.techDesc}>
+              {description}
+            </p>
+          </div>
+        </div>
+        <div className={styles.techImgWrapper}>
+          <Image
+            src={images.portrait}
+            className={styles.techImg}
+            width={515}
+            height={527}
+          />
         </div>
       </div>
     </section>
